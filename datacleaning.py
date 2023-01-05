7c1079410674ccb83fd4d21a8f7e6ae26d3ab156
predatorq <54240007+predatorq@users.noreply.github.com>
2023-01-05 10:22:10 +0800

    update

--- datacleaning.py
+++ datacleaning.py
@@ -9,17 +9,24 @@ from tqdm import trange
 # nltk.download('stopwords')
 # nltk.download('wordnet')
 
+# read data from csv, the columns are divided by ','
+# use column 'index' as the index column
 issues = pd.read_csv('info.csv', header=None, sep=',', names=['index', 'url', 'title', 'label'], index_col='index')
 
+# do not use the url column
 issues = issues.drop(columns=['url'])
 
+# replace the NaN with 1
 issues.fillna(1, inplace=True)
 
 for i in trange(len(issues)):
+    # only left the english characters
     words = re.sub('[^a-zA-Z]', ' ', issues['title'][i])
     words = (words.lower()).split()
+    # drop the stopwords
     words = [w for w in words if w not in set(stopwords.words('english'))]
 
+    # lemmatize
     lemma = WordNetLemmatizer()
     words = [lemma.lemmatize(w) for w in words if len(w) > 1]
 
